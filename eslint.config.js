2c6a9f717e8b5902a5156714cc7331c074fe94d0
doyotteee <doyotteee@gmail.com>
2025-06-01 03:25:35 +0700

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        Buffer: 'readonly',
+        global: 'readonly',
+      },
+    },
+    rules: {
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'eol-last': 'error',
+      'no-unused-vars': 'error',
+      'no-console': 'off',
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-multiple-empty-lines': ['error', { 'max': 1 }],
+      'space-before-function-paren': ['error', 'never'],
+    },
+  },
+];
